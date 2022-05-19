e2c04fdb8ace1a51c30bb952ce4d1c9a23a26cb1
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-19 15:36:14 +0300

    Added function flash color

--- scripts/memory-game.js
+++ scripts/memory-game.js
@@ -14,37 +14,33 @@ function startgame() {
 
         var element = document.getElementById("0");
         element.style.backgroundColor = "black";
-        setTimeout(function() {
-            element.style.backgroundColor = "green";
-        }, 250);
+        flash_color("green");
     } else if (random == 1) {
         play_audio("red.mp3");
 
         var element = document.getElementById("1");
-        element.style.backgroundColor = "black";
-        setTimeout(function() {
-            element.style.backgroundColor = "red";
-        }, 250);
+
+        flash_color("red");
     } else if (random == 2) {
         play_audio("yellow.mp3");
 
         var element = document.getElementById("2");
-        element.style.backgroundColor = "black";
-        setTimeout(function() {
-            element.style.backgroundColor = "yellow";
-        }, 250);
+        flash_color("yellow");
     } else {
         play_audio("blue.mp3");
-
         var element = document.getElementById("3");
-        element.style.backgroundColor = "black";
-        setTimeout(function() {
-            element.style.backgroundColor = "blue";
-        }, 250);
+        flash_color("blue");
     }
 
     function play_audio(a) {
         var audio = new Audio("../assets/sounds/" + a);
         audio.play();
+    }
+
+    function flash_color(c) {
+        element.style.backgroundColor = "black";
+        setTimeout(function() {
+            element.style.backgroundColor = c;
+        }, 250);
     }
 }
