a11b203e0cf2e7135efc7d73245ed32db2a55d0a
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-19 16:49:10 +0300

    Added window on load

--- scripts/memory-game.js
+++ scripts/memory-game.js
@@ -1,8 +1,12 @@
+window.addEventListener("load", random_generator, false);
 var random = 5;
-document.addEventListener("keypress", function(event) {
-    random = getRandomInt(4);
-    startgame();
-});
+
+function random_generator() {
+    document.addEventListener("keypress", function(event) {
+        random = getRandomInt(4);
+        startgame();
+    });
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -11,19 +15,15 @@ function getRandomInt(max) {
 function startgame() {
     if (random == 0) {
         play_audio("green.mp3");
-
         var element = document.getElementById("0");
         element.style.backgroundColor = "black";
         flash_color("green");
     } else if (random == 1) {
         play_audio("red.mp3");
-
         var element = document.getElementById("1");
-
         flash_color("red");
     } else if (random == 2) {
         play_audio("yellow.mp3");
-
         var element = document.getElementById("2");
         flash_color("yellow");
     } else {
