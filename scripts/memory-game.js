371aa8f731a11e2c0af8fc8fb7a9f5ab33511eff
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-23 12:42:33 +0300

    Added the functions to play the game(though not everything is working well yet)

--- scripts/memory-game.js
+++ scripts/memory-game.js
@@ -1,11 +1,10 @@
-window.addEventListener("load", start_game, false);
+window.addEventListener("load", first_generation, false);
 var random = 5;
+var level_counter = 0;
+var answer = true;
 
-function random_generator() {
-    document.addEventListener("keypress", function(event) {
-        random = getRandomInt(4);
-        choose_div();
-    });
+function first_generation() {
+    document.addEventListener("keypress", start_game);
 }
 
 function getRandomInt(max) {
@@ -13,6 +12,7 @@ function getRandomInt(max) {
 }
 
 function choose_div() {
+    level_counter++;
     if (random == 0) {
         play_audio("green.mp3");
         var element = document.getElementById("0");
@@ -32,11 +32,6 @@ function choose_div() {
         flash_color("blue");
     }
 
-    function play_audio(a) {
-        var audio = new Audio("../assets/sounds/" + a);
-        audio.play();
-    }
-
     function flash_color(c) {
         element.style.backgroundColor = "black";
         setTimeout(function() {
@@ -45,13 +40,49 @@ function choose_div() {
     }
 }
 
+function play_audio(a) {
+    var audio = new Audio("../assets/sounds/" + a);
+    audio.play();
+}
+var user_clicks_array = [];
+var array_of_randoms = [];
+
 function start_game() {
-    for (var i = 0; i < 15; i++) {
-        random_generator();
-        for (var j = 0; j < i; j++) {
-            document.addEventListener("click", function(e) {
-                console.log(e.target.id);
-            });
+    random = getRandomInt(4);
+    array_of_randoms.push(random);
+    choose_div();
+    document.removeEventListener("keypress", start_game);
+
+    function save_user_clicks(e) {
+        if (e.target.id == 0) play_audio("green.mp3");
+        else if (e.target.id == 1) play_audio("red.mp3");
+        else if (e.target.id == 2) play_audio("yellow.mp3");
+        else play_audio("blue.mp3");
+        user_clicks_array.push(e.target.id);
+        console.log(e.target.id);
+
+        if (check_answer() == false)
+            document.getElementById("press").innerHTML("Game Over");
+        else {
+            user_clicks_array = [];
+            start_game();
+            //bede ontor luserclicks laysero add l array of randoms b3den b3mil choosediv jdede
+            // if (user_clicks_array.length === array_of_randoms.length) choose_div();
         }
     }
+
+    document.addEventListener("click", save_user_clicks, false);
+}
+
+function check_answer() {
+    console.log(array_of_randoms);
+    console.log(user_clicks_array);
+    for (var i = 0; i < user_clicks_array.length; i++) {
+        if (array_of_randoms[i] !== parseInt(user_clicks_array[i])) {
+            answer = false;
+        } else {
+            answer = true;
+        }
+    }
+    return answer;
 }
